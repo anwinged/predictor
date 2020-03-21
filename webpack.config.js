3aee2ac8ab1031d2f02005f998f8fa330a9f05a5
Anton Vakhrushev <anwinged@ya.ru>
2020-03-21 20:05:17 +0300

    Add circle ci

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
 const path = require('path');
-const merge = require('deepmerge');
 
-const baseConfig = {
+module.exports = (env = {}) => ({
+    mode: env.production ? 'production' : 'development',
+    entry: path.resolve(__dirname, 'src/index.ts'),
+    output: {
+        filename: env.production ? 'predictor.min.js' : 'predictor.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'predictor',
+        libraryTarget: 'umd',
+        umdNamedDefine: true,
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
     module: {
         rules: [
             {
@@ -10,41 +21,6 @@ const baseConfig = {
                     loader: 'ts-loader',
                 },
             },
-        ]
+        ],
     },
-};
-
-const buildConfig = {
-    mode: 'production',
-    entry: path.resolve(__dirname, 'src/index.ts'),
-    output: {
-        filename: 'predictor.min.js',
-        path: path.resolve(__dirname, 'built'),
-        library: 'predictor',
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-};
-
-const devConfig = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.ts'),
-    output: {
-        filename: 'predictor.js',
-        path: path.resolve(__dirname, 'built'),
-        library: 'predictor',
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-};
-
-module.exports = merge(
-    baseConfig,
-    process.env.WEBPACK_ENV === 'build' ? buildConfig : devConfig
-);
+});
